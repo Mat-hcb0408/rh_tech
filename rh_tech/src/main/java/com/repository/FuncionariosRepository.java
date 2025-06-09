dc565feee5d33cb827c3fb96d6c3a5fb27df371c
Matheus Henrique C Barbosa <matheus.henrique.campos0408@gmail.com>
2025-06-09 19:30:17 -0300

    Delete rh_tech directory

--- rh_tech/src/main/java/com/repository/FuncionariosRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rh_tech.rh_tech.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.model.FuncionariosModel;
-
-@Repository
-public interface FuncionariosRepository extends JpaRepository<FuncionariosModel,Long>{}
--- rh_tech/src/test/java/com/rh_tech/rh_tech/model/FuncionariosModel.java
+++
@@ -1,5 +0,0 @@
-package com.rh_tech.rh_tech.model;
-
-public class FuncionariosModel {
-
-}
